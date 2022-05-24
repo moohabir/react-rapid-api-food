eb011ac53f043d10acad5c54b5e3e15174fae8e0
moohabir <87937848+moohabir@users.noreply.github.com>
2022-05-25 00:00:42 +0200

    s

--- src/App.js
+++ src/App.js
@@ -3,29 +3,27 @@ import "./styles.css";
 
 export default function App() {
   const [endPoints, setEndPoints] = useState("");
-  const [movies, setMovies] = useState([]);
+  const [films, setFilms] = useState([]);
+  const [finalpoints, setFinalpoints] = useState("");
 
   useEffect(() => {
-    getMovies();
+    getMovies(endPoints);
   }, [endPoints]);
 
-  const getMovies = () => {
-    fetch(`https://1mdb-data-searching.p.rapidapi.com/om?q=+${endPoints}`, {
+  const getMovies = (endPoints) => {
+    const options = {
       method: "GET",
       headers: {
-        "X-RapidAPI-Host": "1mdb-data-searching.p.rapidapi.com",
+        "X-RapidAPI-Host": "movies-app1.p.rapidapi.com",
         "X-RapidAPI-Key": "bc8006d48amsh69e5064484e98b7p16d73bjsn37edd8bac393"
       }
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setMovies(data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    };
+
+    fetch("https://movies-app1.p.rapidapi.com/api/movies", options)
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .then((data) => setFilms(data))
+      .catch((err) => console.error(err));
   };
 
   function changeHandler(e) {
@@ -33,6 +31,7 @@ export default function App() {
   }
   const submitHandler = (e) => {
     e.preventDefaoult();
+    setFilms(endPoints);
   };
 
   return (
@@ -41,10 +40,6 @@ export default function App() {
         <input type="text" value={endPoints} onChange={changeHandler} />
         <button type="submit">Submit</button>
       </form>
-      {movies.map((movie, index) => {
-        return <div key={index}></div>;
-      })}
-      ;
     </div>
   );
 }
